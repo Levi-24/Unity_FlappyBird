48b92383b65907b8754b7b49002106f5177a8c7c
Levi-24 <leventemajor36@gmail.com>
2023-11-22 14:03:31 +0100

    asd

--- Assets/Scrips/PipeSpawner.cs
+++ Assets/Scrips/PipeSpawner.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class PipeSpawner : MonoBehaviour
 {
+    [SerializeField] private float _maxTime = 1.5f;
+    [SerializeField] private float _heightRange = 0.45f;
+    [SerializeField] private GameObject _pipe;
+
+    private float _timer; 
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Spawn();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (_timer > _maxTime)
+        {
+            Spawn();
+            _timer = 0;
+        }
+
+        _timer += Time.deltaTime;
+    }
+
+    private void Spawn()
+    {
+        Vector3 spawnPos = transform.position + new Vector3(0, Random.Range(-_heightRange, +_heightRange));
+        GameObject pipe = Instantiate(_pipe, spawnPos, Quaternion.identity);
+
+        Destroy(pipe, 10);
     }
 }
